4533f70516019a555d985dbf32b7b541a493a5d5
Thomas Schmidt <schmidt.thomas0306@gmail.com>
2025-11-13 22:21:18 +0100

    Update friends.js

--- js/friends.js
+++ js/friends.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     loadFriendsAndRequests(); 
-<<<<<<< HEAD
     
     window.setInterval(loadFriendsAndRequests, 3000); 
 
-=======
-    window.setInterval(loadFriendsAndRequests, 2000); 
->>>>>>> 4dec08d30ee646377f7d56be7d6162da44afe690
 });
 
 async function loadFriendsAndRequests() {
